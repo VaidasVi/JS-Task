9f0a844a2230e045fd643b245b2eb9ce08e7f903
VaidasVi <vaidas.sub@gmail.com>
2022-09-28 19:00:57 +0300

    first task completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,35 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  document.getElementById("output").innerHTML = "";
+
+  const kg = Number(document.getElementById("search").value);
+
+  // Converting kg to lb, g, oz
+
+  const lb = (kg * 2.2046).toFixed(2);
+  const g = (kg / 0.001).toFixed(2);
+  const oz = (kg * 35.274).toFixed(2);
+
+  // Creating elements for outputing conversion results
+  const conversionOutput = document.createElement("div");
+  conversionOutput.setAttribute("id", "conversion");
+
+  const h3 = document.createElement("h3");
+  h3.innerText = "Converted Values:";
+
+  const lbOutput = document.createElement("p");
+  const gOutput = document.createElement("p");
+  const ozOutput = document.createElement("p");
+
+  lbOutput.innerText = `1. ${kg} kilograms is equal to ${lb} pounds.`;
+  gOutput.innerText = `2. ${kg} kilograms is equal to ${g} grams.`;
+  ozOutput.innerText = `3. ${kg} kilograms is equal to ${lb} ounces.`;
+
+  conversionOutput.append(h3, lbOutput, gOutput, ozOutput);
+
+  document.getElementById("output").append(conversionOutput);
+});
