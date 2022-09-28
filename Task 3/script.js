9d87108670106805ac9b2c2cb9072467c35f1039
VaidasVi <vaidas.sub@gmail.com>
2022-09-28 19:46:19 +0300

    third task completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,43 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const generateCards = (cards) => {
+  const outputContainer = document.getElementById("output");
+  outputContainer.innerHTML = "";
+
+  //   document.getElementById("message").style.display = "none";
+
+  cards.forEach((card) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", card.avatar_url);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    const userName = document.createElement("h2");
+    userName.classList.add("user-name");
+    userName.innerText = card.login;
+
+    container.append(userName);
+    cardDiv.append(img, container);
+
+    outputContainer.append(cardDiv);
+  });
+};
+
+document.getElementById("btn").addEventListener("click", async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    console.log(data);
+
+    generateCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
